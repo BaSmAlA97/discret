3f475627f16e4042327dfbd344f289113822a025
BaSmAlA97 <120977520+BaSmAlA97@users.noreply.github.com>
2022-12-19 00:02:48 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z = 0;
+
+            for (x = 2; x <= 100; x++)
+            {
+                for (y = 2; y <= sqrt(x); y++)
+                {
+                    if (x % y == 0)
+                    {
+                        z = 0;
+                    }
+                    if (z == 1 && x != 1)
+                    {
+                        Console.WriteLine(x + "is prime");
+                    }
+                    z = 1;
+                }
+
+               
+            }
+            Console.ReadKey();
+
+
+        }
+        static int sqrt(int x) 
+        {
+            return 0;
+        }
+    }
+}
